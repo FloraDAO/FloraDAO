69ade340d3743f8649b9db4c83198dc9866f74b6
egdiala <dialaegwuchukwu@gmail.com>
2021-07-25 12:38:08 +0100

    new whitepaper and social links

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <v-app :style="{background: $vuetify.theme.themes[theme].background}">
+  <v-app :style="{ background: $vuetify.theme.themes[theme].background }">
     <v-app-bar
-      app hide-on-scroll flat
-       :style="{background: $vuetify.theme.themes[theme].background}"
+      app
+      hide-on-scroll
+      flat
+      :style="{ background: $vuetify.theme.themes[theme].background }"
       dark
     >
       <div class="d-flex align-center">
-        <v-app-bar-nav-icon v-if="isMobile" @click="overlay = !overlay" :ripple="false"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon
+          v-if="isMobile"
+          @click="overlay = !overlay"
+          :ripple="false"
+        ></v-app-bar-nav-icon>
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
@@ -16,7 +22,9 @@
           width="40"
         />
 
-        <v-app-bar-title v-if="!isMobile" class="font-weight-bold text-h5 head">FloraDAO</v-app-bar-title>
+        <v-app-bar-title v-if="!isMobile" class="font-weight-bold text-h5 head"
+          >FloraDAO</v-app-bar-title
+        >
       </div>
 
       <v-spacer></v-spacer>
@@ -37,7 +45,13 @@
         <v-btn plain text :ripple="false">
           <span class="text-capitalize">Vault</span>
         </v-btn>
-        <v-btn :href="`${publicPath}FloraDAO_Whitepaper.pdf`" download="FloraDAO_Whitepaper" plain text :ripple="false">
+        <v-btn
+          :href="`${publicPath}FloraDAO_Whitepaper.pdf`"
+          download="FloraDAO_Whitepaper"
+          plain
+          text
+          :ripple="false"
+        >
           <span class="text-capitalize">Whitepaper</span>
         </v-btn>
         <v-btn plain text :ripple="false">
@@ -46,75 +60,122 @@
       </div>
     </v-app-bar>
 
-
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
-    <v-footer color="#0E0E12"
-      
-      padless
-    >
+    <v-footer color="#0E0E12" padless>
       <v-container>
         <v-row>
-            <v-col color="primary" class="text-justify mt-5" cols="5" xl="5" lg="5">
-              <v-img src="@/assets/img/floradao.jpeg" class="shrink mr-2 br-15" width="80" contain></v-img>
-            </v-col>
-            <v-col color="primary" class="text-justify mt-5" cols="3" md="3">
-              <div v-for="(social, s) in socials" :key="s" class="pa-2 text-body-2">
+          <v-col
+            color="primary"
+            class="text-justify mt-5"
+            cols="5"
+            xl="5"
+            lg="5"
+          >
+            <v-img
+              src="@/assets/img/floradao.jpeg"
+              class="shrink mr-2 br-15"
+              width="80"
+              contain
+            ></v-img>
+          </v-col>
+          <v-col color="primary" class="text-justify mt-5" cols="3" md="3">
+            <div
+              v-for="(social, s) in socials"
+              :key="s"
+              class="pa-2 text-body-2"
+            >
               <a :href="social.url" target="_blank"> {{ social.name }} </a>
-              </div>
-            </v-col>
+            </div>
+          </v-col>
         </v-row>
       </v-container>
     </v-footer>
-     <v-overlay :opacity="opacity" :value="overlay">
-        <v-icon @click="overlay = false" size="1.5x" class="custom-class" style="position: fixed; top: 2%; right: 1.5em;">mdi-close</v-icon>
-        <v-row class="text-center">
-          <v-col cols="12">
-            <v-btn href="/" text plain :ripple="false">
-              <span class="font-weight-light text-capitalize white--text">Home</span>
-            </v-btn>
-          </v-col>
-          <v-col cols="12">
-            <v-btn href="#" text plain :ripple="false">
-              <span class="font-weight-light text-capitalize white--text">About</span>
-            </v-btn>
-          </v-col>
-          <v-col cols="12">
-            <v-btn href="#" text plain :ripple="false">
-              <span class="font-weight-light text-capitalize white--text">FAQ</span>
-            </v-btn>
-          </v-col>
-          <v-col cols="12">
-            <v-btn href="#" text plain :ripple="false">
-              <span class="font-weight-light text-capitalize white--text">Governance</span>
-            </v-btn>
-          </v-col>
-          <v-col cols="12">
-            <v-btn href="#" text plain :ripple="false">
-              <span class="font-weight-light text-capitalize white--text">Vault</span>
-            </v-btn>
-          </v-col>
-          <v-col cols="12">
-            <v-btn :href="`${publicPath}FloraDAO_Whitepaper.pdf`" download="FloraDAO_Whitepaper" text plain :ripple="false">
-              <span class="font-weight-light text-capitalize white--text">Whitepaper</span>
-            </v-btn>
-          </v-col>
-          <v-col cols="12">
-            <v-btn text plain :ripple="false">
-              <span class="font-weight-light text-capitalize white--text">Curators</span>
-            </v-btn>
-          </v-col>
-          <v-col cols="12">
-            <v-btn href="https://t.me/joinchat/8bR3KMe3JAI4Yzg0" target="_blank" color="#FFB39C" outlined rounded>
-              <span class="font-weight-light text-capitalize white--text">Join Community</span>
-            </v-btn>
-          </v-col>
-          <v-col cols="12">
-            <v-btn to="/private_sales" class="px-8" color="#FFB39C" rounded>
-              <span class="font-weight-light text-capitalize white--text">Private Sale</span>
-            </v-btn>
-          </v-col>
+    <v-overlay :opacity="opacity" :value="overlay">
+      <v-icon
+        @click="overlay = false"
+        size="1.5x"
+        class="custom-class"
+        style="position: fixed; top: 2%; right: 1.5em;"
+        >mdi-close</v-icon
+      >
+      <v-row class="text-center">
+        <v-col cols="12">
+          <v-btn href="/" text plain :ripple="false">
+            <span class="font-weight-light text-capitalize white--text"
+              >Home</span
+            >
+          </v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn href="#" text plain :ripple="false">
+            <span class="font-weight-light text-capitalize white--text"
+              >About</span
+            >
+          </v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn href="#" text plain :ripple="false">
+            <span class="font-weight-light text-capitalize white--text"
+              >FAQ</span
+            >
+          </v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn href="#" text plain :ripple="false">
+            <span class="font-weight-light text-capitalize white--text"
+              >Governance</span
+            >
+          </v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn href="#" text plain :ripple="false">
+            <span class="font-weight-light text-capitalize white--text"
+              >Vault</span
+            >
+          </v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn
+            :href="`${publicPath}FloraDAO_Whitepaper.pdf`"
+            download="FloraDAO_Whitepaper"
+            text
+            plain
+            :ripple="false"
+          >
+            <span class="font-weight-light text-capitalize white--text"
+              >Whitepaper</span
+            >
+          </v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn text plain :ripple="false">
+            <span class="font-weight-light text-capitalize white--text"
+              >Curators</span
+            >
+          </v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn
+            href="https://t.me/joinchat/8bR3KMe3JAI4Yzg0"
+            target="_blank"
+            color="#FFB39C"
+            outlined
+            rounded
+          >
+            <span class="font-weight-light text-capitalize white--text"
+              >Join Community</span
+            >
+          </v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn to="/private_sales" class="px-8" color="#FFB39C" rounded>
+            <span class="font-weight-light text-capitalize white--text"
+              >Private Sale</span
+            >
+          </v-btn>
+        </v-col>
       </v-row>
     </v-overlay>
   </v-app>
@@ -125,7 +186,7 @@
 // import { MoonIcon } from 'vue-feather-icons'
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     // SunIcon,
@@ -138,33 +199,36 @@ export default {
     publicPath: process.env.BASE_URL,
     socials: [
       {
-        name: 'Email',
-        url: 'mailto:floradao.io@gmail.com'
+        name: "Email",
+        url: "mailto:floradao.io@gmail.com",
       },
       {
-        name: 'Twitter',
-        url: 'https://twitter.com/FloraDAO2?s=09'
+        name: "Twitter",
+        url: "https://twitter.com/FloraDA0?s=09",
       },
       {
-        name: 'Telegram',
-        url: 'https://t.me/joinchat/8bR3KMe3JAI4Yzg0'
+        name: "Telegram",
+        url: "https://t.me/joinchat/8bR3KMe3JAI4Yzg0",
       },
       {
-        name: 'Github',
-        url: 'https://github.com/FloraDAO/FloraDAO'
+        name: "Instagram",
+        url: "https://www.instagram.com/florada0?r=nametag",
+      },
+      {
+        name: "Github",
+        url: "https://github.com/FloraDAO/FloraDAO",
       },
     ],
   }),
 
-  computed:{
-    theme(){
-      return (this.$vuetify.theme.dark) ? 'dark' : 'light';
-
+  computed: {
+    theme() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
     },
-    isMobile(){
+    isMobile() {
       const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
       return isMobile;
-    }
+    },
   },
 
   mounted() {
@@ -187,12 +251,12 @@ a {
 }
 
 .wave {
-    animation: flow 5s ease-in-out infinite;
-    background: linear-gradient(-60deg, #904e95, #904e95, #e73c7e, #ee7752);
-    background-size: 300%;
-    -webkit-background-clip: text;
-            background-clip: text;
-      -webkit-text-fill-color: transparent;
+  animation: flow 5s ease-in-out infinite;
+  background: linear-gradient(-60deg, #904e95, #904e95, #e73c7e, #ee7752);
+  background-size: 300%;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 @keyframes flow {
   0% {
